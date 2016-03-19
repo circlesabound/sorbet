3005813906f34452ec06324e26400b6b7d7db17d
john <john@circlesabound.com>
2016-03-19 12:33:11 +1100

    Added tests

--- tests/ExchangeTest.rb
+++ tests/ExchangeTest.rb
@@ -3,3 +3,10 @@ require_relative "../lib/Exchange.rb"
 ex = Exchange.connect("development_sandbox")
 while !ex.open?
 end
+
+loop do
+	puts "**************************"
+	puts ex.getDetails
+	puts "**************************"
+	sleep(3)
+end
