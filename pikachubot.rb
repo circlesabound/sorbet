11c5466fe90b82cae813a4f03fb075fb0afa025b
Victor Zhang <victorzhang6897@gmail.com>
2016-03-19 16:08:13 +1100

    test

--- pikachubot.rb
+++ pikachubot.rb
@@ -70,7 +70,7 @@ class PikachuBot
 		@top_10.each_with_index do |sec, index|
 		#every sec return 'buy, sec, (sell price - 1), 100/(index+1)'
 			order = {type: "add", order_id: @counter, symbol: sec, dir: "BUY", price: @sell_book[sec]+1,
-					 size: 100/(index + 1)}
+					 size: 50/(index + 1)}
 			log(order)
 			@buyordercounter += 1
 			@agent.addOrder(order) if @buyordercounter < 10
