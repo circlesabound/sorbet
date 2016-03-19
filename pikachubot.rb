9b20582a3cbdeb2a78f4ed37c426482f19e3d296
Victor Zhang <victorzhang6897@gmail.com>
2016-03-19 14:02:33 +1100

    Buy and sell orders

--- pikachubot.rb
+++ pikachubot.rb
@@ -6,44 +6,65 @@ class PikachuBot
 		@fair_value = {}
 		@current_value = {}
 		@agent = agent
+		@counter = 0
 	end
 
+	
 	def update_fair_values
-		highest_bids = {}
-		lowest_offers = {}
 
 		# get the highest buying price for each security
-		@order_book = @agent.get_order_book.buys
-		@order_book.each do |buy|
-			if highest_bids.has_key? buy.name
-				highest_bids[buy.name] = buy.price if highest_bids[buy.name] < buy.price
-			else
-				highest_bids[buy.name] = buy.price
-			end
-		end
-
-		# get the lowest selling price for each security
-		@order_book = @agent.get_order_book.sells
-		@order_book.each do |sell|
-			if lowest_offers.has_key? sell.name
-				lowest_offers[sell.name] = sell.price if lowest_offers[sell.name] > sell.price
-			else
-				lowest_offers[sell.name] = sell.price
-			end
+		@order_book = @agent.getDetails
+		@order_book.each do |sec, stats|
+			@buy_book[sec] = @stats[buy_price]
+			@sell_book[sec] = @stats[sell_price]
 		end
 
 		# populate fair values with the means
-		common_securities = highest_bids.keys & lowest_offers.keys
+		common_securities = @buy_book.keys & @sell_book.keys
 		common_securities.each do |sec|
-			@fair_value[sec] = [highest_bid[sec], lowest_offers[sec]].reduce(:+).to_f / 2
+			@fair_value[sec] = [@buy_book[sec], @sell_book[sec]].reduce(:+).to_f / 2
 		end
 	end
 
-	def recommended_order
+	def get_fulfilled_buy_orders
+		temp = @agent.getFulfilledOrders
+		new = {}
+		temp.each do |k|
+			new[k] = temp[k] if temp[k][:dir] == "BUY"
+		end
+		new
+	end
+
+	def get_fulfilled_sell_orders
+		temp = @agent.getFulfilledOrders
+		new = {}
+		temp.each do |k|
+			new[k] = temp[k] if temp[k][:dir] == "SELL"
+		end
+		new
+	end
+
+	def recommended_buy_order
 		@top_10 = percentage_difference
 		@top_10.each_with_index do |sec, index|
 		#every sec return 'buy, sec, (sell price - 1), 100/(index+1)'
+			order = {type: "add", dir: "BUY", symbol: sec, price: @sell_book[sec]+1,
+					 size: 100/(index + 1), unique_id: @counter}
+			@buyordercounter += 1
+			@agent.addOrder(order) if @buyordercounter < 10
 		end
+	end
+
+	def recommended_sell_order
+		@gottensecs = @agent.get_fulfilled_sell_orders
+		@gottensecs.each do |id|
+			order = {type: "add", dir: "SELL", symbol: @gottensecs[id][:sec], price: @buy_book[@gottensecs[:sec]]-1,
+				 	size: @gottensecs[id][:size], unique_id: @counter}
+			@buyordercounter = [0, @buyordercounter - 1].max
+			@counter += 1
+			@agent.addOrder(order)
+		end
+		
 	end
 
 	def percentage_difference
--- pikachubotrunner.rb
+++ pikachubotrunner.rb
@@ -13,7 +13,9 @@ class PikachuBotRunner
 		pikachu = PikachuBot.new(@agent)
 		while @agent.open?
 			pikachu.update_fair_values
-			pikachu.recommended_order
+			pikachu.recommended_buy_order
+			pikachu.recommended_sell_order
+			sleep(5)
 		end
 		@agent.close
 	end
