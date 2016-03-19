1bc955f12ef795af38c684470eed2d5f35731c93
john <john@circlesabound.com>
2016-03-19 19:07:06 +1100

    yasdfasdfa

--- bulbasaurbot.rb
+++ bulbasaurbot.rb
@@ -66,15 +66,15 @@ class Bulbasaurbot
 			return nil
 		else
 			@old_prices.each do |symbol, p|
-				puts "New buy price #{@new_prices[symbol][:buy_price]}, old buy price #{@old_prices[symbol][:buy_price]}"
 				if @new_prices[symbol][:buy_price] < @old_prices[symbol][:buy_price]
+					puts "New buy price #{@new_prices[symbol][:buy_price]}, old buy price #{@old_prices[symbol][:buy_price]}"
 					suggestions[symbol][:buy] = true
 					suggestions[symbol][:buy_quantity] = [@new_prices[symbol][:buy_available] - 1, 0].max
 				else
 					suggestions[symbol][:buy] = false
 				end
-				puts "New sell price #{@new_prices[symbol][:sell_price]}, old sell price #{@old_prices[symbol][:sell_price]}"
 				if @new_prices[symbol][:sell_price] > @old_prices[symbol][:sell_price]
+					puts "New sell price #{@new_prices[symbol][:sell_price]}, old sell price #{@old_prices[symbol][:sell_price]}"
 					suggestions[symbol][:sell] = true
 					suggestions[symbol][:sell_quantity] = [@new_prices[symbol][:sell_available] - 1, 0].max
 				else
